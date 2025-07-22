a162e22f76a4992c175eb02a62ff83740e6c7e93
Kaká Xavier Sena <kakaxsena@gmail.com>
2025-07-22 12:50:49 -0300

    two way binding with v-model

--- learning-material/01-vue-core-concepts/app.js
+++ learning-material/01-vue-core-concepts/app.js
@@ -1,27 +1,22 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 10,
+      counter: 0,
       name: "",
-      confirmedName: "",
     };
   },
   methods: {
-    confirmName() {
-      this.confirmedName = this.name;
-    },
-    submitForm() {
-      alert("submitted");
-    },
-    setName(event) {
+    setName(event, lastName) {
       this.name = event.target.value;
     },
-
     add(num) {
       this.counter = this.counter + num;
     },
-    sub(num) {
+    reduce(num) {
       this.counter = this.counter - num;
+    },
+    resetInput() {
+      this.name = "";
     },
   },
 });
